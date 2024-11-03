147e0cfc94b45e686234d0b426b56243cc8f2acb
IsmaelBastos <ismaelbastos2006@gmail.com>
2024-11-03 19:35:59 +0100

    update

--- desafios/ds009/function.js
+++ desafios/ds009/function.js
@@ -5,5 +5,5 @@ function reajustar(){
     let aumento = (salario * porcentagem) / 100
     let reajuste = 1 * salario + aumento
     let res = window.document.getElementById("resultado")
-    res.innerHTML=`<p><strong>${nome} recebeu um amento salarial ! </strong></p> <p> O salário actual era ${salario} kz </p> <p>Com um aumento de ${porcentagem}%, o salário vai aumentar ${aumento} kz no proximo mês.</p> <p>E a partir daí, ${nome} vai passar a ganhar ${reajuste}.</p>`
+    res.innerHTML =`<p><strong>${nome} recebeu um amento salarial ! </strong></p> <p> O salário actual era ${salario} kz </p> <p>Com um aumento de ${porcentagem}%, o salário vai aumentar ${aumento} kz no proximo mês.</p> <p>E a partir daí, ${nome} vai passar a ganhar ${reajuste} kz.</p>`
 }
